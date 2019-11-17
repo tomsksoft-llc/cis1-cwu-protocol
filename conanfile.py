696af99fa95cc4379b9c218ad90eb996dbff17d2
Innokentiy Mokin <enjection@ya.ru>
2019-11-17 11:14:37 +0700

    update protocol and libs

--- conanfile.py
+++ conanfile.py
@@ -4,14 +4,14 @@ from conans import CMake
 
 class Cis1CWUProtocol(ConanFile):
     name = "cis1_cwu_protocol"
-    version = "0.0.1"
+    version = "0.0.4"
     description = "Protocol part of CWU protocol for cis1."
     author = "MokinIA <mia@tomsksoft.com>"
     generators = "cmake"
     settings = "os", "arch", "compiler", "build_type"
     exports = []
     exports_sources = ["CMakeLists.txt", "include/*"]
-    requires = ("cis1_tpl/0.0.1@tomsksoft/cis1")
+    requires = ("cis1_tpl/0.0.3@tomsksoft/cis1")
 
     def build(self):
         cmake = CMake(self)
--- include/protocol.h
+++ include/protocol.h
@@ -11,12 +11,39 @@ namespace cis1
 namespace cwu
 {
 
+struct session_auth
+{
+    std::string session_token;
+    std::string session_id;
+    uint64_t pid;
+    uint64_t ppid;
+
+    static constexpr auto get_converter()
+    {
+        using namespace reflect;
+        return make_meta_converter<session_auth>()
+            .set_name(
+                    CT_STRING("cwu"),
+                    CT_STRING("session_auth"))
+            .add_field(
+                    CT_STRING("session_token"),
+                    ptr_v<&session_auth::session_token>())
+            .add_field(
+                    CT_STRING("session_id"),
+                    ptr_v<&session_auth::session_id>())
+            .add_field(
+                    CT_STRING("pid"),
+                    ptr_v<&session_auth::pid>())
+            .add_field(
+                    CT_STRING("ppid"),
+                    ptr_v<&session_auth::ppid>())
+            .done();
+    }
+};
+
 struct log_entry
 {
     std::chrono::time_point<std::chrono::system_clock> time;
-    std::optional<std::string> session_id;
-    uint64_t pid;
-    uint64_t ppid;
     std::string message;
 
     static constexpr auto get_converter()
@@ -24,22 +51,14 @@ struct log_entry
         using namespace reflect;
         return make_meta_converter<log_entry>()
             .set_name(
+                    CT_STRING("cwu"),
                     CT_STRING("log_entry"))
             .add_field(
                     CT_STRING("time"),
                     ptr_v<&log_entry::time>())
             .add_field(
-                    CT_STRING("session_id"),
-                    ptr_v<&log_entry::session_id>())
-            .add_field(
                     CT_STRING("message"),
                     ptr_v<&log_entry::message>())
-            .add_field(
-                    CT_STRING("pid"),
-                    ptr_v<&log_entry::pid>())
-            .add_field(
-                    CT_STRING("ppid"),
-                    ptr_v<&log_entry::ppid>())
             .done();
     }
 };
